6fa63c3684e2281760afbd96e97d5f8fb0fcd5ed
Zhengxin Zeng <zhze@microsoft.com>
2020-07-27 19:48:43 +0800

    Add project files.

---
+++ SortAlgorithm/MainEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainEntry
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
