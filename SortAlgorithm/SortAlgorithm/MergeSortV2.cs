3de46f4b4c15e9c16df7e93a71e0641565582ea7
MasterofPLM <15321487307@163.com>
2020-08-23 23:20:05 +0800

    Add MergeSortV2 and test

---
+++ SortAlgorithm/SortAlgorithm/MergeSortV2.cs
@@ -0,0 +1,108 @@
+﻿namespace SortAlgorithm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class MergeSortV2 : SortMethod
+    {
+        private ArrayPrinter ap = new ArrayPrinter();
+        private InsertSort insertSort = new InsertSort();
+
+        public override void Sort(int[] inputArray)
+        {
+            if (inputArray == null)
+            {
+                return;
+            }
+
+            Merge(inputArray, 0, inputArray.Length - 1);
+        }
+
+        private void Merge(int[] inputArray, int start, int end, int k = 4)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+
+            int mid = start + (end - start) / 2;
+            int[] left = new int[mid - start + 1];
+            int[] right = new int[end - mid];
+
+            if (mid - start + 1 > k)
+            {
+                Merge(inputArray, start, mid);
+                ap.CopyArrayToArray(inputArray, start, mid, left);
+            }
+            else
+            {
+                ap.CopyArrayToArray(inputArray, start, mid, left);
+                this.insertSort.Sort(left);
+            }
+            
+            if (end - mid > k)
+            {
+                Merge(inputArray, mid + 1, end);
+                ap.CopyArrayToArray(inputArray, mid + 1, end, right);
+            }
+            else
+            {
+                ap.CopyArrayToArray(inputArray, mid + 1, end, right);
+                this.insertSort.Sort(right);
+            }
+
+            int ind1 = 0;
+            int ind2 = 0;
+            int ind = start;
+            int len1 = mid - start + 1;
+            int len2 = end - mid;
+            while (ind1 < len1 && ind2 < len2)
+            {
+                if (left[ind1] <= right[ind2])
+                {
+                    inputArray[ind] = left[ind1];
+                    ind1++;
+                    ind++;
+                }
+                else
+                {
+                    inputArray[ind] = right[ind2];
+                    ind2++;
+                    ind++;
+                }
+
+                if (ind1 == len1)
+                {
+                    if (ind2 < len2)
+                    {
+                        ap.CopyArrayToArray(
+                            right,
+                            ind2,
+                            len2 - 1,
+                            inputArray,
+                            ind,
+                            ind + len2 - ind2 - 1);
+                    }
+                    ind1++;
+                }
+
+                if (ind2 == len2)
+                {
+                    if (ind1 < len1)
+                    {
+                        ap.CopyArrayToArray(
+                            left,
+                            ind1,
+                            len1 - 1,
+                            inputArray,
+                            ind,
+                            ind + len1 - ind1 - 1);
+                    }
+                    ind2++;
+                }
+            }
+            return;
+        }
+    }
+}
---
+++ SortAlgorithm/SortAlgorithmTests/MergeSortV2Tests.cs
@@ -0,0 +1,104 @@
+﻿namespace SortAlgorithm.Tests
+{
+    using SortAlgorithm;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class MergeSortV2Tests
+    {
+        private MergeSortV2 mergeSort;
+        private ArrayGenerator arrayGenerator;
+        private int[] testArray;
+        private int Length = 10;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.mergeSort = new MergeSortV2();
+            this.arrayGenerator = new ArrayGenerator();
+        }
+
+        [Test]
+        public void BaseTest()
+        {
+            ArrayPrinter arrayPrinter = new ArrayPrinter();
+            this.arrayGenerator.RandomGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+            arrayPrinter.Print(this.testArray);
+            this.mergeSort.Sort(this.testArray);
+            arrayPrinter.Print(this.testArray);
+
+            for (int i = 0; i < this.Length - 1; i++)
+            {
+                Assert.LessOrEqual(this.testArray[i], this.testArray[i + 1]);
+            }
+        }
+
+        [Test]
+        public void SpeedTest()
+        {
+            this.Length = 100000;
+            this.arrayGenerator.RandomGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+
+            var startTime = DateTime.Now;
+            this.mergeSort.Sort(this.testArray);
+            var endTime = DateTime.Now;
+
+            var dur = endTime.Subtract(startTime);
+            Console.WriteLine("Timespan: {0} ms.", dur.TotalMilliseconds);
+        }
+
+        [Test]
+        public void WorstSpeedTest()
+        {
+            this.Length = 100000;
+            this.arrayGenerator.DescendGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+
+            var startTime = DateTime.Now;
+            this.mergeSort.Sort(this.testArray);
+            var endTime = DateTime.Now;
+
+            var dur = endTime.Subtract(startTime);
+            Console.WriteLine("Timespan: {0} ms.", dur.TotalMilliseconds);
+        }
+
+        [Test]
+        public void BestSpeedTest()
+        {
+            this.Length = 100000;
+            this.arrayGenerator.AscendGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+
+            var startTime = DateTime.Now;
+            this.mergeSort.Sort(this.testArray);
+            var endTime = DateTime.Now;
+
+            var dur = endTime.Subtract(startTime);
+            Console.WriteLine("Timespan: {0} ms.", dur.TotalMilliseconds);
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            this.Length = 0;
+            this.arrayGenerator.RandomGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+            this.mergeSort.Sort(this.testArray);
+            Assert.IsEmpty(this.testArray);
+
+            this.Length = 1;
+            this.arrayGenerator.RandomGenerator(this.Length);
+            this.testArray = this.arrayGenerator.output;
+            this.mergeSort.Sort(this.testArray);
+            Assert.AreEqual(this.testArray.Length, 1);
+            Assert.AreEqual(this.testArray[0], this.arrayGenerator.output[0]);
+
+            this.testArray = null;
+            this.mergeSort.Sort(this.testArray);
+            Assert.IsNull(this.testArray);
+        }
+    }
+}
